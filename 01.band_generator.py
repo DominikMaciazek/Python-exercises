6d87f8d3e5c1197614ce9c8ac2e746b8427dccd2
Dominik Maciazek <114284333+DominikMaciazek@users.noreply.github.com>
2022-10-30 20:55:01 +0100

    Add files via upload

---
+++ 01.band_generator.py
@@ -0,0 +1,7 @@
+def band_generator():
+    print ("Welcome to the Band Name Generator\n ")
+    name_of_city = input("What's name of the city you grew up in?\n")
+    pet_name = input("What's your pet's name?")
+    print(f"Your band name could be: {name_of_city} {pet_name}")
+
+band_generator()
